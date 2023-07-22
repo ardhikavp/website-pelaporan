1ba887e4831f9e37271c55ee8a704cd9a3feeb8a
nuril <ahmadnuril@student.ppns.ac.id>
2023-07-22 19:13:33 +0700

    nomor laporan factory

--- database/factories/SafetyObservationFormFactory.php
+++ database/factories/SafetyObservationFormFactory.php
@@ -6,7 +6,9 @@ use App\Models\User;
 use App\Models\Image;
 
 use App\Models\Location;
+use Illuminate\Support\Carbon;
 use Illuminate\Http\UploadedFile;
+use App\Models\SafetyObservationForm;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 
@@ -26,8 +28,45 @@ class SafetyObservationFormFactory extends Factory
         $imageIds = Image::pluck('id')->all();
         $pegawaiUsers = User::where('role', 'pegawai')->pluck('id')->all();
 
+                // Ambil bulan dan tahun saat ini
+        $now = Carbon::now();
+        $month = $now->format('m');
+        $year = $now->format('Y');
+
+        // Ambil laporan terakhir dalam bulan tersebut
+        $lastReport = SafetyObservationForm::whereMonth('created_at', '=', $month)
+            ->whereYear('created_at', '=', $year)
+            ->orderByDesc('id')
+            ->first();
+
+        // Buat nomor laporan berikutnya
+        if ($lastReport) {
+            $lastNumber = intval(substr($lastReport->nomor_laporan, 0, 3));
+            $newNumber = $lastNumber + 1;
+            $nomorLaporan = str_pad($newNumber, 3, '0', STR_PAD_LEFT);
+        } else {
+            $nomorLaporan = '001';
+        }
+
+        // Ambil singkatan perusahaan dari input
+        $locationId = $this->faker->randomElement($locationIds);
+        $locationName = Location::find($locationId)->location;
+        $abbreviation = '';
+
+        $words = explode(" ", $locationName);
+
+        foreach ($words as &$word) {
+            preg_match_all('/[A-Z]/', $word, $matches);
+            $abbreviation .= implode("", $matches[0]);
+        }
+
+        $abbreviation = str_pad($abbreviation, 5);
+
+        // Buat string nomor laporan
+        $nomorLaporanString = $nomorLaporan . '/' . 'SBC' . '/' . $abbreviation . '/' . $month . '/' . $year;
+
         return [
-            'nomor_laporan' => $this->faker->unique()->word,
+            'nomor_laporan' => $nomorLaporanString,
             'date_finding' => $this->faker->date(),
             'location_id' => $this->faker->randomElement($locationIds),
             'safety_observation_type' => $this->faker->randomElement(['unsafe_action', 'unsafe_condition', 'bad_housekeeping']),
