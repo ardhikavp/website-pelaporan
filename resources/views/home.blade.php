be14fcbbdab2ea7d032642bacf52f322f57ade98
fadelananda <fadelananda123@gmail.com>
2023-07-29 22:30:25 +0700

    fix bar chart

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,46 +1,50 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-12">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-12">
+                <div class="card">
+                    <div class="card-header">{{ __('Dashboard') }}</div>
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
+                    <div class="card-body">
+                        @if (session('status'))
+                            <div class="alert alert-success" role="alert">
+                                {{ session('status') }}
+                            </div>
+                        @endif
 
-                    {{ __('You are logged in!') }}
-                </div>
-                <div class="row"></div>
+                        {{ __('You are logged in!') }}
+                    </div>
+                    <div class="row"></div>
                     <div class="col-md-6">
                         <canvas id="pieChart" width="50%" height="50%"></canvas>
                     </div>
+                    <div class="col-md-6">
+                        <canvas id="barChart" width="50%" height="50%"></canvas>
+                    </div>
                 </div>
-                    <div class="col">
-                        <div class="card">
-                            <div class="card-header">Project Statistics</div>
-                            <div class="card-body">
-                                <p>Total Companies: {{ $totalCompanies }}</p>
-                                <p>Number of Safety Observations in Each Location:</p>
-                                <ul>
-                                    @foreach ($safetyObservationsPerLocation as $observation)
-                                        <li>{{ $observation->location }}: {{ $observation->total }}</li>
-                                    @endforeach
-                                </ul>
-                                <canvas id="chart-bar" width="400" height="200"></canvas>
-                            </div>
+                <div class="col">
+                    <div class="card">
+                        <div class="card-header">Project Statistics</div>
+                        <div class="card-body">
+                            <p>Total Companies: {{ $totalCompanies }}</p>
+                            <p>Number of Safety Observations in Each Location:</p>
+                            <ul>
+                                @foreach ($safetyObservationsPerLocation as $observation)
+                                    <li>{{ $observation->location }}: {{ $observation->total }}</li>
+                                @endforeach
+                            </ul>
+                            <canvas id="chart-bar" width="400" height="200"></canvas>
                         </div>
                     </div>
+                </div>
             </div>
         </div>
-        <div >
+        <div>
             <div class="card">
-                <div class="card-header"><h1>Laporan</h1>
+                <div class="card-header">
+                    <h1>Laporan</h1>
                     <div class="card-body">
                         <table class="table table-bordered">
                             <thead>
@@ -55,16 +59,16 @@
                                 </tr>
                             </thead>
                             <tbody>
-                                @foreach($data as $item)
-                                <tr>
-                                    <td>{{ $item->tahun }}</td>
-                                    <td>{{ $item->bulan }}</td>
-                                    <td>{{ $item->jumlah_approved }}</td>
-                                    <td>{{ $item->jumlah_rejected }}</td>
-                                    <td>{{ $item->jumlah_pending_review }}</td>
-                                    <td>{{ $item->jumlah_pending_approval }}</td>
-                                    <td>{{ $item->hasil_perhitungan }}</td>
-                                </tr>
+                                @foreach ($data as $item)
+                                    <tr>
+                                        <td>{{ $item->tahun }}</td>
+                                        <td>{{ $item->bulan }}</td>
+                                        <td>{{ $item->jumlah_approved }}</td>
+                                        <td>{{ $item->jumlah_rejected }}</td>
+                                        <td>{{ $item->jumlah_pending_review }}</td>
+                                        <td>{{ $item->jumlah_pending_approval }}</td>
+                                        <td>{{ $item->hasil_perhitungan }}</td>
+                                    </tr>
                                 @endforeach
                             </tbody>
                         </table>
@@ -74,39 +78,46 @@
         </div>
         @include('component.card')
     </div>
-</div>
-@pushOnce('body-scripts')
-<script>
-    var chartData = @json($chartData);
-</script>
-<script>
-    // Get the data from PHP and convert it into separate arrays for labels and data
-    const labels = {!! json_encode($safetyObservationsPerLocation->pluck('location')) !!};
-    const data = {!! json_encode($safetyObservationsPerLocation->pluck('total')) !!};
+    </div>
+    @pushOnce('body-scripts')
+        <script>
+            var chartData = @json($chartData);
+        </script>
+        <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+        <script>
+            const labels = {!! json_encode($safetyObservationsPerLocation->pluck('location')) !!};
+            const data = {!! json_encode($safetyObservationsPerLocation->pluck('total')) !!};
 
-    // Create a new bar chart
-    const ctx = document.getElementById('chart-bar').getContext('2d');
-    const myChart = new Chart(ctx, {
-        type: 'bar',
-        data: {
-            labels: labels,
-            datasets: [{
-                label: 'Number of Safety Observations',
-                data: data,
-                backgroundColor: 'rgba(75, 192, 192, 0.2)', // Customize the bar color
-                borderColor: 'rgba(75, 192, 192, 1)', // Customize the border color
-                borderWidth: 1
-            }]
-        },
-        options: {
-            scales: {
-                y: {
-                    beginAtZero: true // Start the y-axis from 0
-                }
-            }
-        }
-    });
-</script>
-@endPushOnce
+            document.addEventListener("DOMContentLoaded", function() {
+                var ctx = document.getElementById('barChart').getContext('2d');
 
+                // Sample data (replace this with your own data)
+                var data = {
+                    labels: labels,
+                    datasets: [{
+                        label: 'Sample Bar Chart',
+                        data, // Replace this with your data
+                        backgroundColor: 'rgba(75, 192, 192, 0.2)', // Bar fill color
+                        borderColor: 'rgba(75, 192, 192, 1)', // Border color
+                        borderWidth: 1
+                    }]
+                };
+
+                var options = {
+                    responsive: true,
+                    scales: {
+                        y: {
+                            beginAtZero: true
+                        }
+                    }
+                };
+
+                var myBarChart = new Chart(ctx, {
+                    type: 'bar',
+                    data: data,
+                    options: options
+                });
+            });
+        </script>
+    @endPushOnce
 @endsection
